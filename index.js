01ff0c8a60c108d221bb616b5cd4a0a84c477fa5
RUDRANSHFLY <mayamayavir@gmail.com>
2025-05-31 07:04:18 +0530

    added bare miminmum

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import "dotenv/config"
+
+
+
+
+const app = express()
+const PORT = process.env.PORT || 3000  
+
+
+
+app.use(express.json());
+
+
+
+
+
+
+app.listen(PORT,() => {
+    console.log(`Server started 🌟`);
+    console.log(`http://localhost:${PORT}`)
+})
